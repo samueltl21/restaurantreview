9f4fe6c2acb77ee6bc57b3c243533c4def455070
cherry <fff.cherrrrry@gmail.com>
2025-04-26 00:07:27 +0800

    create profile page with profile info and data summary functions

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import application
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 
 @application.route('/')
 def index():
     return render_template('home.html')
+
+@application.route('/profile')
+def profile():
+    return render_template('profile.html')
