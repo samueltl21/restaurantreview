8545b479a3c207c6dc591a57d414d01376393285
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2025-05-01 10:20:49 +0800

    edit models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
+from typing import Optional
+from sqlalchemy import sa
+from sqlalchemy.orm import so
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100),  nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    id : so.Mapped[int] = so.mapped_column(primary_key=True)
+    name : so.Mapped[str] = so.mapped_column(sa.String(100), index=True, unique=True)
+    email : so.Mapped[str] = so.mapped_column(sa.String(50), unique=True, nullable=False)
+    password_hash: so Mapped[Optional[str]] = so.mapped_column(sa.String(100)), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
