4f564050ee1befb577406801bf36337e825bd3a8
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2025-05-02 15:15:39 +0800

    password_hash not optional and name unique set to false

--- app/models.py
+++ app/models.py
@@ -5,9 +5,9 @@ from app import db
 
 class User(db.Model):
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
-    name : so.Mapped[str] = so.mapped_column(sa.String(100), index=True, unique=True)
+    name : so.Mapped[str] = so.mapped_column(sa.String(100), nullable=False)
     email : so.Mapped[str] = so.mapped_column(sa.String(50), unique=True, nullable=False)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(100), nullable=False)
+    password_hash: so.Mapped[str] = so.mapped_column(sa.String(100), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
