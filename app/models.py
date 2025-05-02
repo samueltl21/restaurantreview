3a6d49ff1bfcbf30331c89f6a26832f88685b172
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2025-05-02 10:17:41 +0800

    modify import sa and so

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from typing import Optional
-from sqlalchemy import sa
-from sqlalchemy.orm import so
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 from app import db
 
 class User(db.Model):
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
     name : so.Mapped[str] = so.mapped_column(sa.String(100), index=True, unique=True)
     email : so.Mapped[str] = so.mapped_column(sa.String(50), unique=True, nullable=False)
-    password_hash: so Mapped[Optional[str]] = so.mapped_column(sa.String(100)), nullable=False)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(100), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
