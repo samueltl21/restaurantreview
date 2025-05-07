37de1834c806ea624533ea975ad3bc04d0c05ae3
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2025-05-07 12:22:34 +0800

    add restaurant class

--- app/models.py
+++ app/models.py
@@ -11,3 +11,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.email}>'
+    
+class Restaurant(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(100), nullable=False, unique=True)
+    location: so.Mapped[str] = so.mapped_column(sa.String(150), nullable=False)
+    cuisine: so.Mapped[str] = so.mapped_column(sa.String(50), nullable=False)
+    added_by: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Restaurant {self.name}>"
