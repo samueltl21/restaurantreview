18962cff62ff5206c9341d470580de0d85d9a65d
samueltl21 <162679648+samueltl21@users.noreply.github.com>
2025-05-07 18:21:25 +0800

    remove config from app folder

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_database_location
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
