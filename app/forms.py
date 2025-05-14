a5a94026410e485c21f467e2c4659a71ec7db9dc
ZHMETI <163286968+ZHMETI@users.noreply.github.com>
2025-05-14 20:39:06 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField, DecimalField, SelectField, TextAreaField, Optional
-from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange
+from wtforms import StringField, PasswordField, SubmitField, DateField, DecimalField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, Optional
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=50)])
